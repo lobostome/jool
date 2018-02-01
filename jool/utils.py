52c0b03b7f54af698fab8ad23a6df175b39e9ddc
Peter Damianov <pddamianov@gmail.com>
2018-02-01 12:15:45 -0600

    feat(utils): add change directory context

--- jool/utils.py
+++ jool/utils.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import os
+from contextlib import contextmanager
+
 def constant(f):
     def fset(self, value):
         raise TypeError
     def fget(self):
         return f()
     return property(fget, fset)
+
+@contextmanager
+def cd(new_dir):
+    prev_dir = os.getcwd()
+    try:
+        os.chdir(os.path.expanduser(new_dir))
+        yield
+    finally:
+        os.chdir(prev_dir)
