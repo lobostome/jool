1f58563ec07fcf0beb254df173b3e2b6dafdcdc3
Peter Damianov <pddamianov@gmail.com>
2018-02-04 00:20:02 -0600

    test(git): create a gitrepo fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,22 @@
-import pytest
-import tempfile
-import os
+# -*- coding: utf-8 -*-
+
+import os, pytest
+from jool.utils import cd
+from jool.directory import Location
+from jool.git import Git
 
 @pytest.fixture()
-def cleandir():
-    newpath = tempfile.mkdtemp()
-    os.chdir(newpath)
+def gitrepo():
+    test_repo = "git@github.com:requests/requests.git"
+    cloned_repo = "requests"
+    try:
+        l = Location()
+        l.directory = l.generate_temp_directory_name()
+        l.create_temp_directory()
+
+        g = Git(os.environ['JOOL_PUBLIC_KEY'], os.environ['JOOL_PRIVATE_KEY'])
+        with cd(l.directory):
+            g.clone_repository(test_repo, cloned_repo)
+            yield l.directory, test_repo, cloned_repo
+    finally:
+        l.remove_temp_directory()
--- tests/test_git.py
+++ tests/test_git.py
@@ -1,21 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import os, pytest
-from jool.utils import cd
-from jool.directory import Location
-from jool.git import Git
+import os
 
-def test_clone_repo():
-    test_repo = "git@github.com:requests/requests.git"
-    cloned_repo = "requests"
-    try:
-        l = Location()
-        l.directory = l.generate_temp_directory_name()
-        l.create_temp_directory()
-
-        g = Git(os.environ['JOOL_PUBLIC_KEY'], os.environ['JOOL_PRIVATE_KEY'])
-        with cd(l.directory):
-            g.clone_repository(test_repo, cloned_repo)
-            assert os.path.exists(cloned_repo)
-    finally:
-        l.remove_temp_directory()
+def test_clone_repo(gitrepo):
+    directory, test_repo, cloned_repo = gitrepo
+    assert os.path.exists(cloned_repo)
