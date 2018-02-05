fc42b59233c11b79d3d88be1cfb93cf239313879
Peter Damianov <pddamianov@gmail.com>
2018-02-05 01:01:46 -0600

    feat(git): get project history

--- jool/git.py
+++ jool/git.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 
-import pygit2
+from pygit2 import Keypair, RemoteCallbacks, Repository, clone_repository, GIT_SORT_TOPOLOGICAL, GIT_SORT_REVERSE
 
 class Git(object):
-    def __init__(self, public_key, private_key):
+    def __init__(self, public_key, private_key, repo_from=None, repo_to=None):
         self.public_key = public_key
         self.private_key = private_key
 
-    def clone_repository(self, repo_from, repo_to):
-        keypair = pygit2.Keypair("git", self.public_key, self.private_key, "")
-        callbacks = pygit2.RemoteCallbacks(credentials=keypair)
+    def clone_repo(self, repo_from, repo_to):
+        self.repo_from = repo_from
+        self.repo_to = repo_to
+        keypair = Keypair("git", self.public_key, self.private_key, "")
+        callbacks = RemoteCallbacks(credentials=keypair)
 
-        pygit2.clone_repository(repo_from, repo_to, callbacks=callbacks)
+        clone_repository(self.repo_from, self.repo_to, callbacks=callbacks)
+
+    def traverse(self):
+        repo = Repository('%s/.git' % self.repo_to)
+        commits = []
+        for commit in repo.walk(repo.head.target, GIT_SORT_REVERSE):
+            commits.append(commit.id)
+
+        return commits
